e0e33a06ea52ab4d7b8cd206c4ce07002381c5ef
Elias Manuel Nuñez <tzanu007@gmail.com>
2026-05-13 22:48:53 -0300

    GenreModel Completo

--- app/models/genres.model.php
+++ app/models/genres.model.php
@@ -5,6 +5,7 @@ class GenresModel{
 
   public function getGenres(){
     $db = Database::getConnection();
+    //te sedo esta, me gusta como manejas la conexion
     $query = $db->prepare('SELECT * FROM genero');
     $query->execute();
     return $query->fetchAll();
@@ -15,5 +16,23 @@ class GenresModel{
     $query = $db->prepare('SELECT * FROM genero WHERE id_genero = ?');
     $query->execute([$id]);
     return $query->fetch();
+  }
+
+  public function insertGenre($nombre,$descripcion,$imagen){
+    $db = Database::getConnection();
+    $query = $db->prepare('INSERT INTO genero (nombre,descripcion,imagen) VALUES (?,?,?)');
+    $query->execute([$nombre,$descripcion,$imagen]);
+  }
+
+  public function updateGenre($id,$nombre,$descripcion,$imagen){
+    $db = Database::getConnection();
+    $query = $db->prepare('UPDATE genero SET nombre = ?, descripcion = ?, imagen = ? WHERE id_genero = ?');
+    $query->execute([$nombre,$descripcion,$imagen,$id]);
+  }
+
+  public function deleteGenre($id){
+    $db= Database::getConnection();
+    $query = $db->prepare('DELETE FROM genero WHERE id_genero = ?');
+    $query->execute([$id]);
   }
 }
